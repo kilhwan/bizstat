23fa8e7ffe5b085e16862d1b11876c449c2b9911
Kilhwan Kim <sys4ppl@gmail.com>
2020-04-07 21:55:35 +0900

    Book links are added and checker is just tested.

--- R/tutorial.R
+++ R/tutorial.R
@@ -3,3 +3,18 @@ exercise_topics <- list.files("inst/tutorials/")
 run_exercise <- function(topic) {
   learnr::run_tutorial(topic, package = "bizstatp")
 }
+
+check_exercise <- function(
+  label,
+  user_code,
+  solution_code,
+  check_code,
+  envir_result,
+  evaluate_result,
+  envir_prep,
+  ...
+) {
+  list(message = "답이 저장되었습니다.",
+       correct = TRUE, location = "append")
+}
+
